78753d329e6f286e85c11a0062dbad70f4cf4f42
AhmedElnaggar <ahmedalnaggar1995@gmail.com>
2020-10-26 14:21:26 +0200

    refactoring service interfaces to base interface. Closes #10

--- Pet-Clinic-Data/src/main/java/learning/springbootframework/petclinic/services/VetService.java
+++ Pet-Clinic-Data/src/main/java/learning/springbootframework/petclinic/services/VetService.java
@@ -2,10 +2,6 @@ package learning.springbootframework.petclinic.services;
 
 import learning.springbootframework.petclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet,Long> {
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet owner);
-    Set<Vet> findAll();
 }
